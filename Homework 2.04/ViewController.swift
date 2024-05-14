0186f4cdd613ac565e4dace0abf8f680e64fd6ea
Daniil Maiboroda <Dant2603@gmail.com>
2024-05-15 02:25:11 +0300

    Connected slider elements to code and implemented logic for slider updates with corresponding text fields.

--- Homework 2.04/ViewController.swift
+++ Homework 2.04/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var sliderControlledView: UIView!
+    
+    @IBOutlet var redColorTextField: UITextField!
+    @IBOutlet var greenColorTextField: UITextField!
+    @IBOutlet var blueColorTextField: UITextField!
+    
+    @IBOutlet var redColorSlider: UISlider!
+    @IBOutlet var greenColorSlider: UISlider!
+    @IBOutlet var blueColorSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sliderControlledView.layer.cornerRadius = 8
     }
-
-
+    
+    
+    
+    @IBAction func redColorSliderAction() {
+        redColorTextField.text = String(format: "%.2f", redColorSlider.value)
+    }
+    
+    @IBAction func greenColorSliderAction() {
+        greenColorTextField.text = String(format: "%.2f", greenColorSlider.value)
+    }
+    
+    @IBAction func blueColorSliderAction() {
+        blueColorTextField.text = String(format: "%.2f", blueColorSlider.value)
+    }
 }
 
