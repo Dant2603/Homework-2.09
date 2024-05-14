6de2ef0819f92c35923447d0664e2fddfc939bce
Daniil Maiboroda <Dant2603@gmail.com>
2024-05-15 02:41:23 +0300

    Completed final implementation of the application logic. Added method to handle view color changes upon slider movement. Adjusted initial values for views and text fields.

--- Homework 2.04/ViewController.swift
+++ Homework 2.04/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var sliderControlledView: UIView!
+    @IBOutlet var colorView: UIView!
     
     @IBOutlet var redColorTextField: UITextField!
     @IBOutlet var greenColorTextField: UITextField!
@@ -22,21 +22,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        sliderControlledView.layer.cornerRadius = 8
+        colorView.layer.cornerRadius = 8
     }
     
     
     
     @IBAction func redColorSliderAction() {
         redColorTextField.text = String(format: "%.2f", redColorSlider.value)
+        setColorView()
     }
     
     @IBAction func greenColorSliderAction() {
         greenColorTextField.text = String(format: "%.2f", greenColorSlider.value)
+        setColorView()
     }
     
     @IBAction func blueColorSliderAction() {
         blueColorTextField.text = String(format: "%.2f", blueColorSlider.value)
+        setColorView()
+    }
+    
+    private func setColorView() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
     }
 }
 
