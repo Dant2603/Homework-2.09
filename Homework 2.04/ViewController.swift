9fa976e0f70198f1ee57ac746e24150124f673e8
Daniil Maiboroda <Dant2603@gmail.com>
2024-05-15 00:04:21 +0300

    Initial Commit

---
+++ Homework 2.04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.04
+//
+//  Created by Мария Гетманская on 15.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
