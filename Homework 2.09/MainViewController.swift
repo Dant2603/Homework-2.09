c86eb2f364a461941fe11419c56d3642fe623e40
Daniil Maiboroda <Dant2603@gmail.com>
2024-09-13 22:36:19 +0300

    Implemented color transfer from the main screen to the settings screen and back using delegation. Added text fields for RGB input with a numeric keyboard, including toolbar with Done button. Integrated text field validation and synchronization with sliders and labels. Added validation with UIAlertController for incorrect inputs and ensured sliders update when transferring color between screens.

--- Homework 2.09/MainViewController.swift
+++ Homework 2.09/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var colorView: UIView!
     
@@ -19,14 +19,83 @@ class MainViewController: UIViewController {
     @IBOutlet var greenColorSlider: UISlider!
     @IBOutlet var blueColorSlider: UISlider!
     
+    weak var delegate: ColorDelegate?
+    
+    var passedColor: UIColor?
+    
+    var redValue: Float?
+    var greenValue: Float?
+    var blueValue: Float?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorView.layer.cornerRadius = 8
+        colorView.backgroundColor = passedColor
+        
+        redColorSlider.value = redValue!
+        greenColorSlider.value = greenValue!
+        blueColorSlider.value = blueValue!
+        
+        redColorTextField.text = String(format: "%.2f", redValue!)
+        greenColorTextField.text = String(format: "%.2f", greenValue!)
+        blueColorTextField.text = String(format: "%.2f", blueValue!)
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(dismissKeyboard))
+        
+        toolbar.setItems([doneButton], animated: false)
+        
+        redColorTextField.inputAccessoryView = toolbar
+        greenColorTextField.inputAccessoryView = toolbar
+        blueColorTextField.inputAccessoryView = toolbar
+        
+        redColorTextField.delegate = self
+        greenColorTextField.delegate = self
+        blueColorTextField.delegate = self
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+                view.addGestureRecognizer(tapGesture)
     }
     
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text, let value = Float(text.replacingOccurrences(of: ",", with: ".")) else {
+                showAlert(title: "Invalid Input", message: "Please enter a valid number between 0 and 1.")
+                textField.text = ""
+                
+                return
+            }
+            
+            if value < 0.0 || value > 1.0 {
+                showAlert(title: "Out of Range", message: "Please enter a number between 0 and 1.")
+                textField.text = ""
+                return
+            }
+        
+        if textField == redColorTextField {
+            redColorSlider.value = Float(redColorTextField.text ?? "") ?? 0.0
+        } else if textField == greenColorTextField {
+            greenColorSlider.value = Float(greenColorTextField.text ?? "") ?? 0.0
+        } else if textField == blueColorTextField {
+            blueColorSlider.value = Float(blueColorTextField.text ?? "") ?? 0.0
+        }
+        
+        setColorView()
+    }
     
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
     @IBAction func redColorSliderAction() {
         redColorTextField.text = String(format: "%.2f", redColorSlider.value)
         setColorView()
@@ -41,9 +110,31 @@ class MainViewController: UIViewController {
         blueColorTextField.text = String(format: "%.2f", blueColorSlider.value)
         setColorView()
     }
+        
+    @IBAction func doneButtonPressed(_ sender: UIButton) {
+        if let color = colorView.backgroundColor {
+                delegate?.didChooseColor(color)
+            }
+        dismiss(animated: true, completion: nil)
+    }
     
     private func setColorView() {
         colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
     }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if string == "," {
+            textField.text = (textField.text ?? "") + "."
+            return false
+        }
+        return true
+    }
+    
+    
+
 }
 
+// MARK: - Protocols
+protocol ColorDelegate: AnyObject {
+    func didChooseColor(_ color: UIColor)
+}
--- Homework 2.09/StartViewController.swift
+++ Homework 2.09/StartViewController.swift
@@ -7,23 +7,46 @@
 
 import UIKit
 
-class StartViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class StartViewController: UIViewController, ColorDelegate {
+    
+    @IBOutlet var startView: UIView!
+    
+    var backgroungColor: UIColor?
+    
+    func didChooseColor(_ color: UIColor) {
+        startView.backgroundColor = color
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getRGBComponents(from color: UIColor) -> (red: Float, green: Float, blue: Float)? {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        if color.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return (Float(red), Float(green), Float(blue))
+        } else {
+            return nil
+        }
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startView.backgroundColor = UIColor.white
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let secondVC = segue.destination as? MainViewController {
+            backgroungColor = startView.backgroundColor
+            secondVC.passedColor = backgroungColor
+            secondVC.delegate = self
+            if let rgbComponents = getRGBComponents(from: backgroungColor!) {
+                        secondVC.redValue = rgbComponents.red
+                        secondVC.greenValue = rgbComponents.green
+                        secondVC.blueValue = rgbComponents.blue
+                    }
+        }
+    }
+        
 }
